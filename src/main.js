6e6776bb9cc2f334b2f24dcc56cbe1c8425b78e8
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-08-01 21:08:52 +0300

    toutes les touche de controlle sont fonctionnel

--- src/main.js
+++ src/main.js
@@ -97,7 +97,25 @@ const squares =[]
     generate()
   }
   function moveDown(){
-  
+    for(let i=0;i<4;i++){
+        let totalOne = squares[i].innerHTML;
+        let totalTwo = squares[i + width].innerHTML;
+        let totalThree = squares[i+width*2].innerHTML;
+        let totalfour = squares[i+width*3].innerHTML;
+
+        let column = [parseInt(totalOne),parseInt(totalTwo),parseInt(totalThree),parseInt(totalfour)]
+        
+
+        let filteredColumn = column.filter(num=>num);
+        let missing = 4 - filteredColumn.length
+        let zeros = Array(missing).fill(0);
+        let newColumn=zeros.concat(filteredColumn)
+        
+        squares[i].innerHTML = newColumn[0];
+        squares[i+width].innerHTML = newColumn[1];
+        squares[i+width*2].innerHTML = newColumn[2];
+       squares[i+width*3].innerHTML = newColumn[3];
+    }
   }
   function keydown(){
     moveDown()
