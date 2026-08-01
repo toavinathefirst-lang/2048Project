289bfcb3db3449bfd9952afc2f68e6ec1d859e89
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-08-01 20:39:33 +0300

    ajout du move vers droite

--- src/main.js
+++ src/main.js
@@ -9,7 +9,9 @@ document.addEventListener('DOMContentLoaded',()=>{
   /**
    * @type {HTMLDivElement[]}
    */
-  let squares =[]
+const squares =[]
+
+  let score = 0;
   //create the playing board
   function createBoard (){
     for (let i  = 0; i  < width*width; i++){
@@ -46,10 +48,65 @@ document.addEventListener('DOMContentLoaded',()=>{
 
         let row = [parseInt(totalOne),parseInt(totalTwo),parseInt(totalThree),parseInt(totalfour)]
         console.log(row);
+
+        let filteredRow = row.filter(num=>num);
+        let missing = 4 - filteredRow.length
+        let zeros = Array(missing).fill(0);
+        let newRow=zeros.concat(filteredRow)
         
+        squares[i].innerHTML = newRow[0];
+        squares[i+1].innerHTML = newRow[1];
+        squares[i+2].innerHTML = newRow[2];
+        squares[i+3].innerHTML = newRow[3];
       }
     }
   }
-  moveRight()
+  function checkForWin(){
+
+  }
+   function combineRow(){
+    for(let i=0;i<15;i++){
+      if(squares[i].innerHTML === squares[i+1].innerHTML){
+        let combinedTotal = parseInt(squares[i].innerHTML)*2;
+        squares[i].innerHTML =combinedTotal;
+        squares[i+1].innerHTML =0;
+        score += combinedTotal;
+        scoreDisplay.innerHTML=score
+        
+      }
+    }
+    checkForWin()
+  }
+  function keyRight(){
+    moveRight()
+    combineRow()
+    moveRight()
+    generate()
+  }
+  //assign functions to keys
+  /**
+   * 
+   * @param {KeyboardEvent} e 
+   */
+  function control(e){
+    if(e.key === "ArrowRight"){
+       keyRight()
+    }else if(e.key === 'Arrowleft'){
+      //keyLeft()
+    }
+  }
+
+  document.addEventListener('keydown',control)
+  function moveLeft(){
+
+  }
+  // function keyLeft(){
+  //   moveLeft()
+  //   combineRow()
+  //   moveLeft()
+  //   generate()
+  // }
   
+ 
+
 })
